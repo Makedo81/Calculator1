a896eea3d82594724d8a70f870fa1d67afbca1f8
Makedo81 <domi.stan@wp.pl>
2020-03-03 17:51:11 +0100

    adding created GUI in javaFX

---
+++ src/main/java/CalculatorLayout.java
@@ -0,0 +1,117 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import java.util.List;
+
+public class CalculatorLayout {
+
+    private Display display = new Display();
+    private TextField textField = display.getTextField();
+
+    private Button options[] = new Button[]{
+            new Button("1"),
+            new Button("2"),
+            new Button("3"),
+            new Button("="),
+            new Button("4"),
+            new Button("5"),
+            new Button("6"),
+            new Button("C"),
+            new Button("7"),
+            new Button("8"),
+            new Button("9"),
+            new Button("0"),
+            new Button("+"),
+            new Button("-"),
+            new Button("*"),
+            new Button("/"),
+
+    };
+
+    private Button one = options[0];
+    private Button two = options[1];
+    private Button three = options[4];
+    private Button four = options[5];
+    private Button five = options[5];
+    private Button six = options[6];
+    private Button seven = options[8];
+    private Button eight = options[9];
+    private Button nine = options[10];
+    private Button zero = options[11];
+    private Button add = options[12];
+    private Button substract = options[13];
+    private Button multiply = options[14];
+    private Button divide = options[15];
+    private Button result = options[3];
+    private Button delete = options[7];
+
+    public void setButtonOne(Button options[]) {
+
+        one.setOnAction(e -> {
+            display.getTextField().appendText(one.getText());
+        });
+        two.setOnAction(e -> {
+            textField.appendText(two.getText());
+        });
+        three.setOnAction(e -> {
+            textField.appendText(three.getText());
+        });
+        four.setOnAction(e -> {
+            textField.appendText(four.getText());
+        });
+        five.setOnAction(e -> {
+            display.getTextField().appendText(five.getText());
+        });
+        six.setOnAction(e -> {
+            textField.appendText(six.getText());
+        });
+        seven.setOnAction(e -> {
+            textField.appendText(seven.getText());
+        });
+        eight.setOnAction(e -> {
+            textField.appendText(eight.getText());
+        });
+        nine.setOnAction(e -> {
+            textField.appendText(nine.getText());
+        });
+        zero.setOnAction(e -> {
+            textField.appendText(zero.getText());
+        });
+        add.setOnAction(e -> {
+            textField.appendText(add.getText());
+        });
+        substract.setOnAction(e -> {
+            textField.appendText(substract.getText());
+        });
+        multiply.setOnAction(e -> {
+            textField.appendText(multiply.getText());
+        });
+        divide.setOnAction(e -> {
+            textField.appendText(divide.getText());
+        });
+        delete.setOnAction(e -> {
+            textField.clear();
+        });
+        result.setOnAction(e -> {
+            try {
+                ArrayCreator arrayCreator = new ArrayCreator();
+                List<String> list = arrayCreator.createArray(display.getTextField().getText());
+                Calculation calculation = new Calculation();
+                double result = calculation.calculateResult(list);
+                display.getTextField().clear();
+                display.getTextField().setText(String.valueOf(result));
+            } catch (ArithmeticException o) {
+                System.out.println("Some values try to be divide by '0'");
+                textField.clear();
+                textField.setText(" error ");
+            } catch (IllegalStateException o) {
+                System.out.println("Wrong equation pattern.Try new equation");
+                textField.clear();
+                textField.setText(" error ");
+            }
+        });
+    }
+
+    public Button[] getOptions() {
+        return options;
+    }
+}
---
+++ src/main/java/Display.java
@@ -0,0 +1,54 @@
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
+
+public class Display {
+
+    private FlowPane flowpane = new FlowPane();
+    private FlowPane flowpane1 = new FlowPane();
+    private FlowPane flowpane2 = new FlowPane();
+    private FlowPane flowpane3 = new FlowPane();
+    private FlowPane flowpane4 = new FlowPane();
+    private static TextField textField = new TextField();
+
+    public GridPane createCalculatorDisplay() {
+        CalculatorLayout calculatorLayout = new CalculatorLayout();
+        Button[] options = calculatorLayout.getOptions();
+        calculatorLayout.setButtonOne(options);
+        for (int i = 0; i < 4; i++) {
+            options[i].setPrefHeight(100);
+            options[i].setPrefWidth(100);
+            flowpane.getChildren().add(options[i]);
+        }
+        for (int i = 4; i < 8; i++) {
+            options[i].setPrefHeight(100);
+            options[i].setPrefWidth(100);
+            flowpane1.getChildren().add(options[i]);
+        }
+        for (int i = 8; i < 12; i++) {
+            options[i].setPrefHeight(100);
+            options[i].setPrefWidth(100);
+            flowpane2.getChildren().add(options[i]);
+        }
+        for (int i = 12; i < 16; i++) {
+            options[i].setPrefHeight(100);
+            options[i].setPrefWidth(100);
+            flowpane3.getChildren().add(options[i]);
+        }
+
+        flowpane4.getChildren().add(textField);
+        textField.setPrefSize(400, 100);
+        GridPane gridPane = new GridPane();
+        gridPane.add(flowpane4, 1, 1, 1, 1);
+        gridPane.add(flowpane, 1, 2, 1, 1);
+        gridPane.add(flowpane1, 1, 3, 1, 1);
+        gridPane.add(flowpane2, 1, 4, 1, 1);
+        gridPane.add(flowpane3, 1, 5, 1, 1);
+        return gridPane;
+    }
+
+    public TextField getTextField() {
+        return textField;
+    }
+}
